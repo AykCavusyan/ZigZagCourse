52ceee1699cbb67cc018459c18bbe28d8e977cd6
QBOMONTI\Sebastian <ayk@quantheque.com>
2021-03-07 21:55:37 +0300

    Ball Moving

---
+++ ZigZagNewGae/Assets/Scripts/BallController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    Rigidbody rb;
+
+    bool started;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(started);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!started)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                rb.velocity = new Vector3(speed, 0, 0);
+                started = true;
+            }
+        }
+        else if (Input.GetMouseButtonDown(0))
+        {
+            SwitchDirection();
+        }
+
+    }
+
+    void SwitchDirection()
+    {
+        if(rb.velocity.z > 0)
+        {
+            rb.velocity = new Vector3(speed, 0, 0);
+        }
+        else if (rb.velocity.x > 0)
+        {
+            rb.velocity = new Vector3(0, 0, speed);
+        }
+    }
+}
